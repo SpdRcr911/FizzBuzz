aa4e6c5acb0e5ed1f889d4db39d14a2c68c29b82
rafael@delgado.com <rafael@delgado.com>
2020-01-06 21:25:15 -0800

    Add initial Test
    ShouldPrintNumber

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -3,9 +3,19 @@ using FizzBuzz.Library;
 
 namespace FizzBuzz.Tests
 {
-    [TestClass]
-    public class FizzBuzzTests
-    {
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        private FizzBuzzService _fizzBuzz;
+        public FizzBuzzTests()
+        {
+            _fizzBuzz = new FizzBuzzService();
+        }
 
-    }
+        [TestMethod]
+        public void ShouldPrintNumber()
+        {
+            Assert.AreEqual("1", _fizzBuzz.Print(1));
+        }
+    }
 }
